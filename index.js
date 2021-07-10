a5badc4fdc3c07ff41683003645fa05c2ff0a666
Laurenz1606 <root@vmd67862.contaboserver.net>
2021-07-11 00:02:18 +0200

    colorconversion/randomcolors/regex

--- index.js
+++ index.js
@@ -119,9 +119,9 @@ function convertType(value, to) {
       .replace(/%/g, "")
       .split(",");
     const [r, g, b] = HSLToRGB(
-      map(str1, 0, 360, 0, 1),
-      map(str2, 0, 100, 0, 1),
-      map(str2, 0, 100, 0, 1)
+      map(parseInt(str1), 0, 360, 0, 1),
+      map(parseInt(str2), 0, 100, 0, 1),
+      map(parseInt(str3), 0, 100, 0, 1)
     );
     if (to === "hex") {
       return `#${parseInt(r).toString(16)}${parseInt(g).toString(16)}${parseInt(
@@ -139,9 +139,9 @@ function convertType(value, to) {
       .replace(/%/g, "")
       .split(",");
     const [r, g, b] = HSLToRGB(
-      map(str1, 0, 360, 0, 1),
-      map(str2, 0, 100, 0, 1),
-      map(str2, 0, 100, 0, 1)
+      map(parseInt(str1), 0, 360, 0, 1),
+      map(parseInt(str2), 0, 100, 0, 1),
+      map(parseInt(str3), 0, 100, 0, 1)
     );
     if (to === "hex") {
       return `#${parseInt(r).toString(16)}${parseInt(g).toString(16)}${parseInt(
@@ -202,17 +202,18 @@ function HSLToRGB(h, s, l) {
   if (s == 0) {
     r = g = b = l; // achromatic
   } else {
-    var hue2rgb = function hue2rgb(p, q, t) {
+    function hue2rgb(p, q, t) {
       if (t < 0) t += 1;
       if (t > 1) t -= 1;
       if (t < 1 / 6) return p + (q - p) * 6 * t;
       if (t < 1 / 2) return q;
       if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
       return p;
-    };
+    }
 
     var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
     var p = 2 * l - q;
+
     r = hue2rgb(p, q, h + 1 / 3);
     g = hue2rgb(p, q, h);
     b = hue2rgb(p, q, h - 1 / 3);
